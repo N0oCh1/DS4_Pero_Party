1e613498bd1960ddfe912eebee8ba78c0016ca53
GAEL PEREIRA <gael.pereira@utp.ac.pa>
2024-09-02 20:58:56 -0500

    contraste

--- Laboratorio2/FrmEnun4.Designer.cs
+++ Laboratorio2/FrmEnun4.Designer.cs
@@ -57,31 +57,37 @@
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(122, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(179, 19);
+            this.label2.Size = new System.Drawing.Size(202, 22);
             this.label2.TabIndex = 1;
             this.label2.Text = "Ingrese un numero";
             // 
             // numNumero
             // 
             this.numNumero.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.numNumero.BackColor = System.Drawing.Color.Gold;
+            this.numNumero.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numNumero.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.numNumero.Location = new System.Drawing.Point(153, 149);
             this.numNumero.Name = "numNumero";
-            this.numNumero.Size = new System.Drawing.Size(113, 20);
+            this.numNumero.Size = new System.Drawing.Size(124, 30);
             this.numNumero.TabIndex = 3;
             this.numNumero.ValueChanged += new System.EventHandler(this.numNumero_ValueChanged);
             // 
             // txtResultado
             // 
             this.txtResultado.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtResultado.BackColor = System.Drawing.Color.SteelBlue;
+            this.txtResultado.BackColor = System.Drawing.Color.Gold;
             this.txtResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtResultado.Location = new System.Drawing.Point(136, 261);
+            this.txtResultado.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtResultado.Location = new System.Drawing.Point(135, 254);
             this.txtResultado.Name = "txtResultado";
             this.txtResultado.ReadOnly = true;
-            this.txtResultado.Size = new System.Drawing.Size(150, 20);
+            this.txtResultado.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtResultado.Size = new System.Drawing.Size(161, 30);
             this.txtResultado.TabIndex = 4;
             // 
             // panel1
@@ -100,13 +106,15 @@
             // btnResultado
             // 
             this.btnResultado.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnResultado.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnResultado.Location = new System.Drawing.Point(153, 175);
+            this.btnResultado.BackColor = System.Drawing.Color.Gold;
+            this.btnResultado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResultado.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResultado.Location = new System.Drawing.Point(151, 195);
             this.btnResultado.Name = "btnResultado";
-            this.btnResultado.Size = new System.Drawing.Size(115, 20);
+            this.btnResultado.Size = new System.Drawing.Size(126, 29);
             this.btnResultado.TabIndex = 5;
             this.btnResultado.Text = "Resultado";
-            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.UseVisualStyleBackColor = false;
             this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
             // 
             // panel2
